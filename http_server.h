691c1f3b222c118d4973cc8fa9f4834df8844486
Timo Nicolai <timonicolai@arcor.de>
2021-11-11 21:02:33 +0100

    Fix HTTPConnection

--- http_server.h
+++ http_server.h
@@ -5,6 +5,7 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 
+#include <chrono>
 #include <cstdint>
 #include <iostream>
 #include <memory>
@@ -14,17 +15,21 @@
 namespace bm
 {
 
-class HTTPConnection
+class HTTPConnection : public std::enable_shared_from_this<HTTPConnection>
 {
-  enum { READ_BUFFER_SIZE = 8192 };
+  static constexpr std::size_t BUFFER_SIZE { 8192 };
+  static constexpr std::chrono::seconds PROCESS_TIMEOUT { 60 };
 
 public:
   HTTPConnection(boost::asio::ip::tcp::socket&& socket)
-  : m_socket(std::move(socket))
+  : m_socket { std::move(socket) },
+    m_socket_timer { m_socket.get_executor(), PROCESS_TIMEOUT }
   {}
 
   void run()
   {
+    auto this_ { shared_from_this() };
+
     boost::beast::http::async_read(
       m_socket,
       m_read_buffer,
@@ -36,11 +41,21 @@ public:
         else
           std::cout << "received request" << std::endl; // XXX
       });
+
+    m_socket_timer.async_wait(
+      [this_](boost::beast::error_code ec)
+      {
+        if (ec)
+          std::cerr << ec.message() << std::endl;
+        else
+          this_->m_socket.close(ec);
+      });
   }
 
 private:
   boost::asio::ip::tcp::socket m_socket;
-  boost::beast::flat_buffer m_read_buffer { READ_BUFFER_SIZE };
+  boost::asio::steady_timer m_socket_timer;
+  boost::beast::flat_buffer m_read_buffer { BUFFER_SIZE };
   boost::beast::http::request<boost::beast::http::dynamic_body> m_request;
 };
 
@@ -66,7 +81,7 @@ public:
 
 private:
   static std::shared_ptr<HTTPConnection> make_connection(
-    boost::asio::ip::tcp::socket &socket)
+    boost::asio::ip::tcp::socket &&socket)
   {
     return std::make_shared<HTTPConnection>(std::move(socket));
   }
@@ -82,7 +97,7 @@ private:
         if (ec)
           std::cerr << ec.message() << std::endl;
         else
-          make_connection(socket)->run();
+          make_connection(std::move(socket))->run();
 
         accept_connections(acceptor, socket);
       });
